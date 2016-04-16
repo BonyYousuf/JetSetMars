1e7acb6a889e79bca79060ecc2e074b435c43e4b
Bony Yousuf <bony.yousuf@gmail.com>
2016-04-16 22:49:16 +0600

    Project Commit

---
+++ Assets/Scripts/ToggleEffects.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ToggleEffects : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.B))
+		{
+			GetComponent<BloomOptimized>().enabled = !GetComponent<BloomOptimized>().enabled;
+			Debug.Log("Bloom: " + (GetComponent<BloomOptimized>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.S))
+		{
+			GetComponent<SunShafts>().enabled = !GetComponent<SunShafts>().enabled;
+			Debug.Log("SunShafts: " + (GetComponent<SunShafts>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.C))
+		{
+			GetComponent<ColorCorrectionCurves>().enabled = !GetComponent<ColorCorrectionCurves>().enabled;
+			Debug.Log("ColorCorrectionCurves: " + (GetComponent<ColorCorrectionCurves>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.D))
+		{
+			GetComponent<DepthOfField>().enabled = !GetComponent<DepthOfField>().enabled;
+			Debug.Log("DepthOfField: " + (GetComponent<DepthOfField>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.V))
+		{
+			GetComponent<VignetteAndChromaticAberration>().enabled = !GetComponent<VignetteAndChromaticAberration>().enabled;
+			Debug.Log("VignetteAndChromaticAberration: " + (GetComponent<VignetteAndChromaticAberration>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.F))
+		{
+			GetComponent<GlobalFog>().enabled = !GetComponent<GlobalFog>().enabled;
+			Debug.Log("GlobalFog: " + (GetComponent<GlobalFog>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyUp(KeyCode.M))
+		{
+			GetComponent<CameraMotionBlur>().enabled = !GetComponent<CameraMotionBlur>().enabled;
+			Debug.Log("CameraMotionBlur: " + (GetComponent<CameraMotionBlur>().enabled ? "On" : "Off"));
+		}
+		else if(Input.GetKeyUp(KeyCode.J))
+		{
+			ThirdPersonCharacter.Instance.setJetPack(!ThirdPersonCharacter.Instance.hasJetPack());
+		}
+	}
+}
